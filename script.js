0f8a1b55023a1243f1ffd3e05559acacc1ba1092
kys <kang_ys_1@naver.com>
2023-10-26 20:49:09 +0900

    js파일 writeTxt() 주석 추가

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 /***** JS 적용하기 START *****/
 /* text_iife.js */
-// 텍스트 작성과 삭제 즉시 실행 함수
+// 글자가 지워지고 입력되는 동작 추가
+
+// 즉시 실행 함수로 정의하여 홈페이지를 열자마자 함수 실행
+// 즉시 실행 함수 형식: (function name(){실행문};)(매개변수);
 (function () {
+  // spanEl 객체에 css선택자가 "main h2 span"인 요소 노드 생성
   const spanEl = document.querySelector("main h2 span");
+  // 배열 요소 생성
   const txtArr = [
     "Web Publisher",
     "Front-End Developer",
@@ -10,8 +15,19 @@
     "UX Designer",
     "Back-End Developer",
   ];
+  // 배열의 인덱스 초기화
   let index = 0;
+  // txtArr 배열에서 index(=0) 요소 하나를 가져와 한 글자마다 ""으로 나누고,
+  // currentTxt에 배열요소 만들기
   let currentTxt = txtArr[index].split("");
+
+  // [writeTxt() sudo]
+  // 1. spanEl 요소에 currentTxt의 왼쪽부터 요소를 추출하여, textContent에 하나 씩 대입
+  // 2. 만약 currentTxt.length가 0이 아니면 (입력할 글자가 남아 있으면)
+  //  2-1. writeTxt 메소드를 Math.floor(Math.random() * 100)=두 자리 정수/1000 초 뒤에 수행
+  // 3. 만약 currentTxt.length가 0이면 (입력할 글자가 남아있지 않으면)
+  //  3-1. currentTxt에 textContent를 한 글자씩 ""로 나눈 것을 대입하고
+  //  3-2. deleteTxt 메소드를 3초 뒤에 수행. (글자 삭제를 3초 뒤에 시작해라.)
   function writeTxt() {
     spanEl.textContent += currentTxt.shift();
     if (currentTxt.length !== 0) {
@@ -21,6 +37,7 @@
       setTimeout(deleteTxt, 3000);
     }
   }
+
   function deleteTxt() {
     currentTxt.pop();
     spanEl.textContent = currentTxt.join("");
